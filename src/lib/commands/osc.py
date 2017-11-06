87ab0d2bfcd9634529b5610e72aa7ed54f28782b
Leó Stefánsson <leostefansson@gmail.com>
2017-11-05 16:41:27 -0800

    successfully parsing OSC command from TwitchBot and sending to Processing Sketch. Datatype validation required.

--- src/lib/commands/osc.py
+++ src/lib/commands/osc.py
@@ -5,28 +5,30 @@ import types
 
 # Create the OSC Client
 client = OSCClient()
-client.connect( ("localhost", 9999) )
+client.connect( ("localhost", 9797) )
 client.print_tracebacks = False
 oscString = "/Twitch/setup"
 msg = OSCMessage(oscString.encode("utf-8", "ignore"))
 client.send(msg)
 
 def osc(args):
+	# TODO: Validate args datatypes
+	# args[0] should be string
+	# args[1] should be int
 	print "received OSC command"
 	oscCommand = args[0]
 	print(oscCommand)
-  	value = args[1]
+  	value = int(args[1])
   	print(value)
+  	
 	usage = '!osc <parameterName> <value>'
 
 	try:
-		print "trying to broadcast OSC message"
-		oscString = '/FromTwitch/'+str(oscCommand)+' '+str(value)
-		print(oscString)
-		msg = OSCMessage(oscString)
-		print(msg)
+		msg = OSCMessage()
+		msg.setAddress('fromTwitch/'+oscCommand)
+		msg.append(value)
 		client.send(msg)
-		return "OSC Message Sent: " + oscString
+		return "OSC Message Sent."
 	except ValueError:
 		return "!osc value error"
 	except:
